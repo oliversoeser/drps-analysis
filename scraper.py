36922b033594cb1ae3d5bdf98ce9bd668e436c8c
Oliver Soeser <o.soeser@icloud.com>
2024-06-28 18:33:13 +0100

    Rename main.py

--- scraper.py
+++ scraper.py
@@ -5,10 +5,12 @@ import re
 
 DATA = "./data/"
 
-SCHOOL_REGEX = "cx_sb_[a-z]{4}.htm"
+SCHOOL_REGEX = "cx_sb_[a-z]{4}\.htm"
 COURSE_REGEX = "cx[a-z]{4}[0-9]{5}\.htm"
 
 schools = re.findall(SCHOOL_REGEX, get("cx_subindex.htm"))
+
+courses_data = {}
 
 for school in schools:
     courses = re.findall(COURSE_REGEX, get(school))
@@ -16,7 +18,11 @@ for school in schools:
     for course in courses:
         try:
             page = extract(get(course))
+            courses_data[course[2:11]] = page
             with open(DATA + course.replace("htm", "json"), "w", encoding="utf-8") as f:
                 f.write(json.dumps(page))
         except:
             print(f"Failed to extract data from {course}, it likely does not exist.")
+
+with open("courses.json", "w", encoding="utf-8") as f:
+    f.write(json.dumps(courses_data))
