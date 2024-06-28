def01657909ff36255aaed1f49828f6034620f65
Oliver Soeser <o.soeser@icloud.com>
2024-06-28 16:22:52 +0100

    Refactor extractor

--- extractor.py
+++ extractor.py
@@ -1,38 +1,38 @@
 from bs4 import BeautifulSoup
 
 def extract(html: str):
-    f = open("keys.txt", "r")
-    keys_text = f.read()
-    f.close()
+    with open("keys.txt", "r") as keyfile:
+        keys_text = keyfile.read()
 
-    keys_list = [line.split(",") for line in keys_text.split("\n")]
-    keys = {key[0]: key[1] for key in keys_list}
+    keys = {key[0]: key[1] for key in [line.split(",") for line in keys_text.split("\n")]}
 
     html = BeautifulSoup(html, features="lxml")
-
-    page = {}
-
     html = html.find_all("table")[2]
 
+    page = {}
     page["title"] = html.find("h1").decode_contents()
 
     for table in html.find_all("table"):
         caption = table.find("caption")
-        if caption != None:
-            title = caption.text
-            if title in keys.keys():
-                section = keys[title]
-                page[section] = {}
-                
-                for row in table.find_all("tr"):
-                    key = ""
-                    for data in row.find_all("td"):
-                        if data.text in keys.keys():
-                            key = keys[data.text]
-                        if key != "":
-                            text = data.text
-                            text = "\n".join([line.strip() for line in text.split("\n")])
-                            text = "\n".join([line for line in text.split("\n") if line.strip() != ""])
-                            page[section][key] = text
-
+        if caption == None:
+            continue
+        title = caption.text
+        if title not in keys.keys():
+            continue
+        section = keys[title]
+        page[section] = {}
+        for row in table.find_all("tr"):
+            key = ""
+            for data in row.find_all("td"):
+                text = data.text
+                # Remove trailing and leading whitespace, as well as empty lines
+                text = "\n".join([line.strip() for line in text.split("\n") if line.strip() != ""])
+                if section == "outcomes":
+                    page[section] = text
+                elif section == "reading":
+                    page[section] = text
+                elif text in keys.keys():
+                    key = keys[text]
+                elif key != "":
+                    page[section][key] = text
     return page
