6d08cef4e2d80b6350ed413034f28dbf3effd779
Oliver Soeser <o.soeser@icloud.com>
2024-06-28 16:30:58 +0100

    Refactor main file

---
+++ main.py
@@ -0,0 +1,22 @@
+from extractor import extract
+from fetcher import get
+import json
+import re
+
+DATA = "./data/"
+
+SCHOOL_REGEX = "cx_sb_[a-z]{4}.htm"
+COURSE_REGEX = "cx[a-z]{4}[0-9]{5}\.htm"
+
+schools = re.findall(SCHOOL_REGEX, get("cx_subindex.htm"))
+
+for school in schools:
+    courses = re.findall(COURSE_REGEX, get(school))
+
+    for course in courses:
+        try:
+            page = extract(get(course))
+            with open(DATA + course.replace("htm", "json"), "w", encoding="utf-8") as f:
+                f.write(json.dumps(page))
+        except:
+            print(f"Course {course} was not read, the URL likely does not exist.")
