51e8b4e70a104425b23ccff6c3ad56b034155765
Oliver Soeser <hello@oliversoeser.com>
2024-12-31 13:06:06 +0100

    Priority Topological Sort Draft Implementation

--- graphs.py
+++ graphs.py
@@ -60,25 +60,50 @@ class PriorityVertex:
         self.id = id
         self.priority = priority
     
+    # Operations are swapped to turn the min heap into a max heap
     def __lt__(self, other):
-        return self.priority < other.priority
+        return self.priority > other.priority
 
     def __gt__(self, other):
-        return self.priority > other.priority
+        return self.priority < other.priority
 
 class PriorityGraph(Graph):
     priorities = []
+    V_heap = []
+    adj_heaps = []
 
     def addVertex(self, priority):
         self.priorities.append(priority)
+        self.adj_heaps.append([])
         return super().addVertex()
     
     def topSort(self):
+        for i, priority in enumerate(self.priorities):
+            heapq.heappush(self.V_heap, PriorityVertex(i, priority))
+
+        for i, adj in enumerate(self.adj):
+            for w in adj:
+                heapq.heappush(self.adj_heaps[i], PriorityVertex(w, self.priorities[w]))
+
         self.state = [WHITE] * self.n
         self.L = []
 
-        for v in range(self.n):
+        while len(self.V_heap) != 0:
+            v = heapq.heappop(self.V_heap).id
             if self.state[v] == WHITE:
                 self.sortFromVertex(v)
         
         return self.L
+    
+    def sortFromVertex(self, v):
+        self.state[v] = GREY
+
+        while len(self.adj_heaps[v]) != 0:
+            w = heapq.heappop(self.adj_heaps[v]).id
+            if self.state[w] == WHITE:
+                self.sortFromVertex(w)
+            elif self.state[w] == GREY:
+                raise GraphCycleError
+        
+        self.state[v] = BLACK
+        self.L.append(v)
