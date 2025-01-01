59cc1616a383ac08d7312965e30058498f62d5cd
Oliver Soeser <hello@oliversoeser.com>
2025-01-01 01:21:53 +0100

    Improved data processing

--- processing.py
+++ processing.py
@@ -1,7 +1,27 @@
 import re
 
 COURSE_ID_PATTERN = r"[A-Z]{4}[0-9]{5}"
+SCQF_PATTERN = r"SCQF Level (?P<level>\d+) \((Year (?P<year>\d) )?(?P<degree>Undergraduate|Postgraduate)\)"
+SEMESTER_PATTERN = r"[Ss]em(ester)? (?P<semester>[12])"
 
 def prereq(s):
     return [x.lower() for x in re.findall(COURSE_ID_PATTERN, s)]
 
+def year(s):
+    m = re.match(SCQF_PATTERN, s)
+    return m.groupdict()["year"]
+
+def level(s):
+    m = re.match(SCQF_PATTERN, s)
+    return m.groupdict()["level"]
+
+def degree(s):
+    m = re.match(SCQF_PATTERN, s)
+    return m.groupdict()["degree"]
+
+def semester(s):
+    m = re.match(SEMESTER_PATTERN, s)
+    if m == None:
+        return 1
+    else:
+        return m.groupdict()["semester"]
